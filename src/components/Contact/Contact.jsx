d3a77f88e170aef46b4223d3246647ad7cd6709f
Abdel Rahman Mansour <65471490+Graycouch@users.noreply.github.com>
2021-05-20 13:52:00 -0500

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,14 @@
 import emailjs from "emailjs-com";
 import React from 'react';
+import handshake from "./1.svg";
+import "./contact.scss";
 
 export default function Contact() {
 
     function sendEmail(e) {
         e.preventDefault();
 
-    emailjs.sendForm('service_rpc2cfr', 'template_hiaibpy', e.target, 'user_lz6D8EjVLqkcPaqTTvcp9')
+    emailjs.sendForm('gmail', 'template_hiaibpy', e.target, 'user_lz6D8EjVLqkcPaqTTvcp9')
         .then((result) => {
             console.log(result.text);
         }, (error) => {
@@ -17,8 +19,12 @@ export default function Contact() {
 
     return(
         <div>
-            <div className="container">
             <form onSubmit={sendEmail}>
+              <div className="contact" id="contact">
+                <div className="left">
+                  <img src= {handshake} className = "" alt="" />
+                </div>
+                  <div className="right">
                     <div className="row pt-5 mx-auto">
                         <div className="col-8 form-group mx-auto">
                             <input type="text" className="form-control" placeholder="Name" name="name"/>
@@ -30,14 +36,15 @@ export default function Contact() {
                             <input type="text" className="form-control" placeholder="Subject" name="subject"/>
                         </div>
                         <div className="col-8 form-group pt-2 mx-auto">
-                            <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message"></textarea>
+                            <textarea className="form-control" id="" cols="50" rows="8" placeholder="Your message" name="message"></textarea>
                         </div>
                         <div className="col-8 pt-3 mx-auto">
                             <input type="submit" className="btn btn-info" value="Send Message"></input>
                         </div>
                     </div>
+                    </div>
+                    </div>
                 </form>
             </div>
-        </div>
     )
 }
